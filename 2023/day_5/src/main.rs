239d33fb18af18ec1b7e67790a195017849e7c08
Miroslav Kovar <miroslav.kovar@absa.africa>
2023-12-20 22:32:36 +0100

    Day 5, part 2 WIP
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

--- 2023/day_5/src/main.rs
+++ 2023/day_5/src/main.rs
@@ -8,6 +8,50 @@ struct MapEntry {
     range: u64,
 }
 
+#[derive(Debug, Clone)]
+struct Seeds {
+    seeds: Vec<u64>,
+    current_start: u64,
+    range: u64,
+    range_remaining: u64,
+}
+
+impl Seeds {
+    pub fn new(mut seeds: Vec<u64>) -> Seeds {
+        let current_start = seeds.remove(0);
+        let range = seeds.remove(0);
+        Seeds {
+            seeds,
+            current_start,
+            range,
+            range_remaining: range,
+        }
+    }
+}
+
+impl Iterator for Seeds {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.range_remaining > 0 {
+            let res = self.current_start + self.range - self.range_remaining;
+            self.range_remaining -= 1;
+            Some(res)
+        } else {
+            if self.seeds.is_empty() {
+                None
+            } else {
+                let current_start = self.seeds.remove(0);
+                let range = self.seeds.remove(0);
+                self.current_start = current_start;
+                self.range = range;
+                self.range_remaining = range - 1;
+                Some(current_start)
+            }
+        }
+    }
+}
+
 fn parse_map_entry(line: &str) -> Result<MapEntry, Box<dyn Error>> {
     let parts: Vec<&str> = line.split_whitespace().collect();
     if parts.len() != 3 {
@@ -27,8 +71,15 @@ fn parse_map_entry(line: &str) -> Result<MapEntry, Box<dyn Error>> {
 
 fn map_source_to_destination(map: &Vec<MapEntry>, value: u64) -> u64 {
     for entry in map.iter() {
-        if value >= entry.source && value <= entry.source + entry.range {
-            return entry.destination + value - entry.source;
+        if value >= entry.source && value < entry.source + entry.range {
+            let res = entry.destination + value - entry.source;
+            // if value == 77 {
+            //     // println!(
+            //     //     "Mapping 77 to {}, the entry is {:?}, map: {:?}",
+            //     //     res, entry, map
+            //     // );
+            // }
+            return res;
         }
     }
     return value;
@@ -54,17 +105,36 @@ fn main() -> Result<(), Box<dyn Error>> {
         maps.push(parsed_section);
     }
 
+    // let mut dests: Vec<u64> = Vec::new();
+    // for seed in &seeds {
+    //     let mut dest: u64 = *seed;
+    //     for map in &maps {
+    //         dest = map_source_to_destination(&map, dest);
+    //     }
+    //     // println!("Mapped seed {} to final destination {}", seed, dest);
+    //     dests.push(dest);
+    // }
+
+    // println!("Minimum of destinations: {}", dests.iter().min().unwrap());
+
+    let actual_seeds = Seeds::new(seeds);
+    // println!("Actual seeds: {:?}", actual_seeds.collect::<Vec<_>>());
+
     let mut dests: Vec<u64> = Vec::new();
-    for seed in seeds {
-        let mut dest = seed;
+    for seed in actual_seeds {
+        // println!("Mapping actual seed {}", seed);
+        let mut dest: u64 = seed;
         for map in &maps {
+            let dest_bef = dest;
             dest = map_source_to_destination(&map, dest);
+            // println!("{} -> {}", dest_bef, dest);
         }
-        println!("Mapped seed {} to final destination {}", seed, dest);
+        // println!("Mapped actual seed {} to final destination {}", seed, dest);
         dests.push(dest);
     }
-
-    println!("Minimum of destinations: {}", dests.iter().min().unwrap());
-
+    println!(
+        "Actual minimum of destinations: {}",
+        dests.iter().min().unwrap()
+    );
     Ok(())
 }
