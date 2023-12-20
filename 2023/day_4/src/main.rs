fd555bc4a0e090b670138d81d79b87950f662f16
Miroslav Kovar <miroslav.kovar@absa.africa>
2023-12-20 15:15:52 +0100

    Day 4, part 2
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

--- 2023/day_4/src/main.rs
+++ 2023/day_4/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     fs::File,
     io::{self, BufRead},
     path::Path,
@@ -10,10 +10,37 @@ fn read_lines<P: AsRef<Path>>(filename: P) -> io::Result<io::Lines<io::BufReader
     Ok(io::BufReader::new(file).lines())
 }
 
+fn insert_or_update(
+    map: &mut HashMap<usize, usize>,
+    key: usize,
+    update_by: usize,
+    card_num: usize,
+) {
+    // println!(
+    //     "Updating {} from card number {} by {}",
+    //     key, card_num, update_by
+    // );
+    match map.get_mut(&key) {
+        Some(val) => {
+            *val += update_by;
+        }
+        None => {
+            map.insert(key, update_by);
+        }
+    }
+}
+
 fn main() {
     let mut sum = 0;
-    for (i, line) in read_lines("./input.txt").unwrap().into_iter().enumerate() {
-        let line = line.unwrap();
+    let mut cards_map: HashMap<usize, usize> = HashMap::new();
+    let lines: Vec<String> = read_lines("./input.txt")
+        .unwrap()
+        .into_iter()
+        .map(|l| l.unwrap())
+        .collect();
+    for (i, line) in lines.iter().enumerate() {
+        let card_num = i + 1;
+        insert_or_update(&mut cards_map, card_num, 1, card_num);
         let card_parts: Vec<&str> = line
             .split(": ")
             .skip(1)
@@ -33,19 +60,25 @@ fn main() {
             .split(" ")
             .filter(|s| !s.is_empty())
             .collect();
-        let mut matches: u32 = 0;
+        let mut matches: usize = 0;
         for number in &my_numbers {
             if winning_numbers.contains(number) {
-                // println!("Matched number {}", number);
+                // println!("Matched number {} on card {}", number, card_num);
                 matches += 1;
             }
         }
-        // println!("Card {} matches: {}", i + 1, matches);
-        if matches > 0 {
-            let inc = 2_u32.pow(matches - 1);
-            // println!("Points: {}", inc);
-            sum += inc;
+        let copies_of_this_card = cards_map.get(&card_num).unwrap().clone();
+        for card_copy_num in 1..matches + 1 {
+            if card_num + card_copy_num <= lines.len() {
+                insert_or_update(
+                    &mut cards_map,
+                    card_num + card_copy_num,
+                    copies_of_this_card,
+                    card_num,
+                );
+            }
         }
     }
-    println!("Sum: {}", sum);
+    // println!("Cards map: {:?}", cards_map.iter());
+    println!("Sum: {}", cards_map.values().sum::<usize>());
 }
