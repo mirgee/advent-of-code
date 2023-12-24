d1de9e13d31ae043fcd71bba142cd39a3b9ad5b6
Miroslav Kovar <miroslav.kovar@absa.africa>
2023-12-24 10:42:26 +0100

    Day 9, part 2
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

--- 2023/day-9/src/part2.rs
+++ 2023/day-9/src/part2.rs
@@ -1,10 +1,32 @@
+use nom::{
+    character::complete::{i64, line_ending, space1},
+    multi::separated_list1,
+    IResult,
+};
+
 use crate::custom_error::AocError;
 
+fn parse_input(input: &str) -> IResult<&str, Vec<Vec<i64>>> {
+    fn parse_line(input: &str) -> IResult<&str, Vec<i64>> {
+        separated_list1(space1, i64)(input)
+    }
+
+    separated_list1(line_ending, parse_line)(input)
+}
+
+fn predict_next_value(mut input: Vec<i64>) -> i64 {
+    let mut first_values = Vec::<i64>::new();
+    while !input.iter().all(|v| v == &0) {
+        first_values.insert(0, input.first().unwrap().clone());
+        input = input.iter().map_windows(|&[a, b]| b - a).collect();
+    }
+    first_values.iter().fold(0, |acc, val| val - acc)
+}
+
 #[tracing::instrument]
-pub fn process(
-    _input: &str,
-) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<i64, AocError> {
+    let (_, values) = parse_input(input).unwrap();
+    Ok(values.into_iter().map(|v| predict_next_value(v)).sum())
 }
 
 #[cfg(test)]
@@ -13,9 +35,10 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+        assert_eq!(2, process(input)?);
         Ok(())
     }
 }
