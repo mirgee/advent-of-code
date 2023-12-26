0f7dbf3b1a792cbb2dd84010cab8d9e5e1f6e9c4
Miroslav Kovar <miroslav.kovar@absa.africa>
2023-12-26 01:11:13 +0100

    Day 11, part 2
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

--- 2023/day-11/src/part2.rs
+++ 2023/day-11/src/part2.rs
@@ -1,8 +1,77 @@
+use itertools::Itertools;
+use ndarray::Array2;
+use nom::{
+    branch::alt,
+    bytes::complete,
+    character::complete::line_ending,
+    multi::{many1, separated_list1},
+    IResult, Parser,
+};
+
 use crate::custom_error::AocError;
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum Item {
+    Space,
+    Galaxy,
+}
+
+fn parse_input(input: &str) -> IResult<&str, Vec<Vec<Item>>> {
+    separated_list1(
+        line_ending,
+        many1(alt((
+            complete::tag(".").map(|_| Item::Space),
+            complete::tag("#").map(|_| Item::Galaxy),
+        ))),
+    )(input)
+}
+
 #[tracing::instrument]
-pub fn process(input: &str) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<u64, AocError> {
+    let (input, output) = parse_input(input).unwrap();
+    let arr = Array2::from_shape_vec(
+        (output.len(), output[0].len()),
+        output.iter().flatten().collect(),
+    )
+    .unwrap();
+    let empty_rows_idx = arr
+        .rows()
+        .into_iter()
+        .enumerate()
+        .filter(|(_i, row)| row.iter().all(|&x| x == &Item::Space))
+        .map(|(i, _row)| i)
+        .collect::<Vec<_>>();
+    let empty_col_idx = arr
+        .columns()
+        .into_iter()
+        .enumerate()
+        .filter(|(_i, col)| col.iter().all(|&x| x == &Item::Space))
+        .map(|(i, _col)| i)
+        .collect::<Vec<_>>();
+    let galaxy_idx = arr
+        .indexed_iter()
+        .filter(|((_row, _col), &x)| x == &Item::Galaxy)
+        .map(|((row, col), _)| (row, col))
+        .collect::<Vec<_>>();
+    let exp_factor = 1_000_000;
+    // TODO: Map galaxy coordinates to expanded coordinates
+    let galaxy_idx = galaxy_idx
+        .iter()
+        .map(|(row, col)| {
+            (
+                row + (exp_factor - 1) * empty_rows_idx.iter().filter(|&&x| x < *row).count(),
+                col + (exp_factor - 1) * empty_col_idx.iter().filter(|&&x| x < *col).count(),
+            )
+        })
+        .collect::<Vec<_>>();
+    let distances = galaxy_idx.iter().combinations(2).map(|x| {
+        let (row1, col1) = x[0].clone();
+        let (row2, col2) = x[1].clone();
+        let row_diff = (row1 as isize - row2 as isize).abs();
+        let col_diff = (col1 as isize - col2 as isize).abs();
+        row_diff as u64 + col_diff as u64
+    });
+    Ok(distances.sum())
 }
 
 #[cfg(test)]
@@ -12,10 +81,21 @@ mod tests {
     use super::*;
 
     #[rstest]
-    #[case("", "")]
+    #[case(
+        "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....",
+        8410
+    )]
     #[test_log::test]
-    fn test_process(#[case] input: &str, #[case] output: &str) -> miette::Result<()> {
-        todo!("haven't built test yet");
+    fn test_process(#[case] input: &str, #[case] output: u64) -> miette::Result<()> {
         assert_eq!(output, process(input)?);
         Ok(())
     }
